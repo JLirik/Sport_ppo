4581175d092929cdcca834e8014557aa20b771fb
P1Jack <118741123+P1Jack@users.noreply.github.com>
2024-12-24 08:59:32 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, url_for, request, render_template, redirect, flash
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'predpof_code_crusaders'
-
-
-if __name__ == '__main__':
-    app.run(port=1024, host='127.0.0.1')
