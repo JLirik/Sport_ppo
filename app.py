5709a5bd9e42f507e36f2951c5400ba239f1b25c
JLirik <dimdimych181007@yandex.ru>
2024-12-24 22:00:02 +0300

    reg-beta-2 commit

--- app.py
+++ app.py
@@ -11,21 +11,29 @@ def home():
 
 @app.route('/registration', methods=['GET'])
 def open_registration():
+    print(8)
     return render_template('register.html')
 
 
 @app.route('/registration', methods=['POST'])
 def regisration():
-    user = request.form['username']
-    password = request.form['password']
-    code = request.form['admin-code']
+    data = request.get_json()
+    print(data)
+    user = data['username']
+    password = data['password']
+    code = data['admin']
     if not user or not password:
         flash('Введены не все данные')
         return redirect('/registration')
+
+    # Проверить на наличие почты в БД - надо функцию
+
     print(user, password)
     if code:
+        # admin
         pass
     else:
+        # user
         pass
 
 
