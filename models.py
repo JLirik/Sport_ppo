982368ec5cd4950f97357291992e8f7bd6113606
P1Jack <118741123+P1Jack@users.noreply.github.com>
2024-12-25 14:54:12 +0300

    Update models.py

--- models.py
+++ models.py
@@ -4,10 +4,34 @@ from flask_login import UserMixin
 db = SQLAlchemy()
 
 
-class User(db.Model, UserMixin):
-    __tablename__ = 'users'
-    email = db.Column(db.String, unique=True, nullable=False, primary_key=True)
-    password = db.Column(db.String, nullable=False)
+class Inventory(db.Model):
+    __tablename__ = 'inventory'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    quality = db.Column(db.String(100), nullable=False)
 
-    def get_id(self):
-        return str(self.email)
+
+class Request(db.Model):
+    __tablename__ = 'requests'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    inventory_id = db.Column(db.Integer, db.ForeignKey('inventory.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    status = db.Column(db.String(50), nullable=False)
+
+
+class Take(db.Model):
+    __tablename__ = 'take'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    inventory_id = db.Column(db.Integer, db.ForeignKey('inventory.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    is_admin = db.Column(db.Boolean, nullable=False)
