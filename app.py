83a406d9ada3cb515ca3f43d19b870476cbdf74e
P1Jack <118741123+P1Jack@users.noreply.github.com>
2024-12-26 12:32:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import *
 from models import *
 from datetime import timedelta
 from flask_login import LoginManager, login_user, logout_user, current_user
-import re
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ppo_pumpkin'
@@ -26,6 +25,11 @@ def load_user(user_id):
 
 @app.route('/', methods=['GET'])
 def home():
+    if current_user.is_authenticated:
+        if current_user.is_admin:
+            return redirect(url_for('admin_main'))
+        return redirect(url_for('user_main'))
+    # create_base_db()
     return render_template('startpage.html')
 
 
@@ -84,7 +88,9 @@ def user_main():
             return redirect(url_for('admin_main'))
     else:
         return redirect(url_for('home'))
-    return render_template('user_main.html')
+    requested_inventory_id = [take.id for take in Take.query.filter_by(user_id=current_user.id).all()]
+    inventory = [[item.id, item.name, item.quantity, item.quality] for item in Inventory.query.filter(Inventory.id.in_(requested_inventory_id)).all()]
+    return render_template('user_main.html', inventory=inventory)
 
 
 @app.route('/admin/main', methods=['GET'])
@@ -103,6 +109,49 @@ def logout():
         logout_user()
         return redirect(url_for('open_login'))
     return redirect(url_for('home'))
+
+
+def create_base_db():
+    to_db_user = User(name='Bob',
+                      password='BlaBla',
+                      is_admin=0)
+    db.session.add(to_db_user)
+    to_db_user = User(name='Bob1',
+                      password='BlaBla',
+                      is_admin=0)
+    db.session.add(to_db_user)
+    to_db_user = User(name='BOB_ADMIN',
+                      password='LoveAlice',
+                      is_admin=1)
+    db.session.add(to_db_user)
+    to_db_inventory = Inventory(name='Сиськи Корибски малые',
+                                quantity=50,
+                                last_quantity=30,
+                                quality='новые')
+    db.session.add(to_db_inventory)
+    to_db_inventory = Inventory(name='Сиськи Корибски большие',
+                                quantity=10,
+                                last_quantity=8,
+                                quality='новые')
+    db.session.add(to_db_inventory)
+    to_db_inventory = Inventory(name='Сиськи Вольвача универсальные',
+                                quantity=50,
+                                last_quantity=40,
+                                quality='новые')
+    db.session.add(to_db_inventory)
+    to_db_take = Take(user_id=1,
+                      inventory_id=1,
+                      quantity=20)
+    db.session.add(to_db_take)
+    to_db_take = Take(user_id=1,
+                      inventory_id=2,
+                      quantity=2)
+    db.session.add(to_db_take)
+    to_db_take = Take(user_id=2,
+                      inventory_id=3,
+                      quantity=10)
+    db.session.add(to_db_take)
+    db.session.commit()
 
 
 if __name__ == '__main__':
