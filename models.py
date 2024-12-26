b18fc555298a0c5b43297d7977a1de10d7fa0435
P1Jack <118741123+P1Jack@users.noreply.github.com>
2024-12-26 12:32:23 +0300

    Update models.py

--- models.py
+++ models.py
@@ -9,6 +9,7 @@ class Inventory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
+    last_quantity = db.Column(db.Integer, nullable=False)
     quality = db.Column(db.String(100), nullable=False)
 
 
