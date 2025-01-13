96ef3f6d31976094d9fd9671227df77c29934fb5
lluneko <117767742+lluneko@users.noreply.github.com>
2025-01-13 15:57:13 +0300

    + user main

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ def user_main():
         if fixes:
             status = fixes.status
         user_inventory.append((product.id, product.name, item.quantity, product.quality, status))
-    return render_template('user_main.html', inventory=user_inventory)
+    return render_template('user-main-2.0.html', inventory=user_inventory)
 
 
 @app.route('/admin/main', methods=['GET'])
